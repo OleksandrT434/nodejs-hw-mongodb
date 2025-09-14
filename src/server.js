1de8a3adfc35967446bc3d1d9707e53612ba0ea4
Aleksandr Tkachenko <prosto.batman1234@gmail.com>
2025-09-14 12:30:30 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.get('/contacts', async (req, res) => {
     res.status(200).json({
       status: 200,
       message: 'Successfully found contacts!',
-      data: { contacts },
+      data: contacts ,
     });
   } catch (error) {
     console.log(error);
@@ -33,7 +33,7 @@ app.get('/contacts/:contactId', async (req, res, next) => {
       res.status(200).json({
         status: 200,
         message: `Successfully found contact with id ${contactId}!`,
-        data: { contact },
+        data:  contact ,
       });
   } catch (error) {
     console.log(error);
